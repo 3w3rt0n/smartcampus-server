3ab0f954d5272bfe1ac2ee3979dc5dc617e00ba3
Ewerton <3w3rt0n@gmail.com>
2018-02-04 14:48:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ HTML_TEMPLATE = Template("""
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js"></script>
   </head>
   <body>
     <div class="jumbotron text-center">
