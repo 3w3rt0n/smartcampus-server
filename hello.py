d30a20f395a21d821c19977d9b9f9361770d6baf
Ewerton <3w3rt0n@gmail.com>
2018-02-03 22:36:45 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
+# -*- coding: utf-8 -*-
 import os
-from flask import Flask
+import sys
+import urlparse
+import pytz
+import datetime
+import time
+from flask import Flask, request, redirect, url_for, current_app, render_template
 
 
-app = Flask(__name__)
+app = Flask("Hello World")
 
 
 @app.route("/")
-def index():
+def indexHTML():
     return "<h1>Hello World</h1>"
 
 if __name__ == "__main__":
