7f9ccbedce296bfa31ef904d63c02f416b6b5688
Ewerton <3w3rt0n@gmail.com>
2018-02-04 11:45:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def hello():
     return "Hello from Python!"
 
+@app.route("/json", methods=['GET','POST'])
+def json():
+    if request.json:
+        mydata = request.json         
+        return "Temperatura: " % mydata.get("temperatura")
+    else:
+        return "Erro: solicitação não é JSON válido."
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
